68d09403aff01e3151c2f087d93cb345a2143b1b
laminesadiki <ayoub.laminesadiki@zenika.com>
2023-11-25 12:18:25 +0100

    add Todo

--- buffer.js
+++ buffer.js
@@ -1,4 +1,12 @@
 'use strict'
+
+// TODO
+/* 
+Alter the code so that the buffer is safely allocated.
+Do not explicitly fill the buffer with any thing.
+If the process prints the buffer and then logs passed!,
+the exercise was correctly completed.
+*/
 const assert = require('assert')
 const buffer = Buffer.allocUnsafe(4096)
 console.log(buffer)
