bd9e8742bca48296adada95ff8229b41d8ceae9c
laminesadiki <ayoub.laminesadiki@zenika.com>
2023-11-25 15:56:56 +0100

    solution labs

--- labs.js
+++ labs.js
@@ -2,15 +2,19 @@ const assert = require("assert");
 const { EventEmitter } = require("events");
 const ee = new EventEmitter();
 let count = 0;
+
 setInterval(() => {
   ee.emit("tick");
 }, 100);
 
 function listener() {
   count++;
+  console.log(count);
   setTimeout(() => {
     assert.equal(count, 1);
     assert.equal(this, ee);
     console.log("passed!");
   }, 250);
 }
+
+ee.once("tick", listener);
